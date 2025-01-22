340042b06af57cb8df826996d084a92daf451111
elfabri <navarro.fabricio.nahuel@gmail.com>
2025-01-22 14:49:56 -0300

    reset ctrl i

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -13,7 +13,6 @@ return {
             vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
             vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
             vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
         end
     }
 }
